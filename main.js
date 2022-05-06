17f224f38bef7f063363128230df2166fc14e866
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-05-06 11:57:11 +0100

    stage 2 pre-start

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/*
+X FETCH DAD JOKE
+X CREATE RANDOM DAD JOKE BUTTON
+- CREATE TOP 10 JOKE LIST
+- INPUT 10 JOKES INTO LIST (1 0R 10 FUNCTION TO POPULATE)
+- STYLE WITH CSS
+- IF ENOUGH TIME, STRETCH JS MORE 
+
+*/
 async function randomDadJoke() {
   let response = await fetch("https://icanhazdadjoke.com/", {
     headers: { accept: "application/json" },
@@ -9,3 +18,18 @@ async function randomDadJoke() {
 
 document.querySelector("#new-joke-button").addEventListener("click", randomDadJoke);
 
+async function firstJokeList() {
+  let response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { accept: "application/json" },
+  });
+  let data = await response.json();
+  return data.joke;
+}
+
+/* 
+- GENERATE EACH LIST ELEMENT THEN SAVE IN VARIABLE.
+- CHANGE LISTS INNER TEXT TO FIRST JOKE ON LIST, THEN SAVE IN VARIABLE.
+- APPENDED LISTS WITH NEW TEXT.
+- APPEND ORDERED LISTS WITH TEXT.
+
+*/
